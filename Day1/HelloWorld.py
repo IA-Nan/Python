bfcc40422ef02dc2d19ef8d750c9a9e5237b78d1
wsn <iwangshengnan@163.com>
2020-11-23 00:12:12 +0800

    master

--- Day1/HelloWorld.py
+++ Day1/HelloWorld.py
@@ -1,6 +1,13 @@
-print("hello world!!!")
-for target_list in range(3):
-    print(target_list)
 
-a = 10
-print(a)
+
+def main():
+
+    print("hello world!!!")
+    for target_list in range(3):
+        print(target_list)
+
+    a = 10
+    print(a)
+
+if __name__ == "__main__":
+    main()
