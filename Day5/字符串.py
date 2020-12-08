ab79c05ac40ac1aa70c67795286d632c7d042a2e
Wsn <iwangshengnan@163.com>
2020-12-08 10:42:28 +0800

    master

--- Day5/字符串.py
+++ Day5/字符串.py
@@ -4,15 +4,15 @@
 # print(a)
 # print(b)
 
-#?查找
+# ?查找
 #! find()
 my_str = 'wo shi wsn ,wo shi yi ge nan sheng'
-print(my_str.find('shi'))  #    3 下标
-print(my_str.find('zzz'))  #    -1
+print(my_str.find('shi'))  # 3 下标
+print(my_str.find('zzz'))  # -1
 
 #! index()
-print(my_str.index('wsn')) #    7 下标
-print(my_str.index('wsn')) #    7 下标
+print(my_str.index('wsn'))  # 7 下标
+print(my_str.index('wsn'))  # 7 下标
 # print(my_str.index('zzz'))   #!报错
 
 #! count()
@@ -20,20 +20,6 @@ print(my_str.count('wo'))           # 2  次数
 print(my_str.count('wo', 10, 20))    # 1次数
 
 
-#?修改
+# ?修改
 
 #! replace()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
